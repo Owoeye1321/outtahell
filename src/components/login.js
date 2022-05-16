5e07f55870ecf0f36bdd1a73d154261fc1d65ff6
Owoeye1321 <owoeye1321@gmail.com>
2022-05-16 13:39:17 -0700

    first commit

---
+++ src/components/login.js
@@ -0,0 +1,10 @@
+import react from "react";
+
+function Login () {
+     return(
+                 <div>
+                     <p>hello world</p>
+                 </div>
+           )
+}
+export default Login;
