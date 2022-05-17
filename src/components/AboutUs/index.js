79ed82411706e2296c3142064b5f6f3b3d95d429
semz-ui <michaeltimilehin36@gmail.com>
2022-05-17 06:33:48 -0500

    sec com

--- src/components/AboutUs/index.js
+++ src/components/AboutUs/index.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import useToggle from '../../Hooks/useToggle';
-import BackToTop from '../BackToTop';
-import FooterHomeOne from '../HomeOne/FooterHomeOne';
-import Drawer from '../Mobile/Drawer';
-import HeaderAbout from './HeaderAbout';
-import HeroAbout from './HeroAbout';
-import ServicesAbout from './ServicesAbout';
-import TeamAbout from './TeamAbout';
+import React from "react";
+import useToggle from "../../Hooks/useToggle";
+import BackToTop from "../BackToTop";
+import FooterHomeOne from "../HomeOne/FooterHomeOne";
+import TeamHomeOne from "../HomeOne/TeamHomeOne";
+import Drawer from "../Mobile/Drawer";
+import HeaderAbout from "./HeaderAbout";
+import HeroAbout from "./HeroAbout";
+import ServicesAbout from "./ServicesAbout";
+import TeamAbout from "./TeamAbout";
 
 function AboutUs() {
-    const [drawer, drawerAction] = useToggle(false);
-    return (
-        <>
-            <Drawer drawer={drawer} action={drawerAction.toggle} />
-            <HeaderAbout drawer={drawer} action={drawerAction.toggle} />
-            <HeroAbout />
-            <ServicesAbout />
-            <TeamAbout />
-            <FooterHomeOne className="appie-footer-about-area" />
-            <BackToTop />
-        </>
-    );
+  const [drawer, drawerAction] = useToggle(false);
+  return (
+    <>
+      <Drawer drawer={drawer} action={drawerAction.toggle} />
+      <HeaderAbout drawer={drawer} action={drawerAction.toggle} />
+      <HeroAbout />
+      <ServicesAbout />
+      <TeamHomeOne />
+      <TeamAbout />
+      <FooterHomeOne className="appie-footer-about-area" />
+      <BackToTop />
+    </>
+  );
 }
 
 export default AboutUs;
