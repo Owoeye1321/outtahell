d019a5dca7dd7d6a2ec6f38729f2766e58b0bdca
Owoeye1321 <owoeye1321@gmail.com>
2022-06-28 11:35:16 -0700

    new commit

--- server/controller/client.js
+++ server/controller/client.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV !== "production") require('dotenv').config();
-   const uri = process.env.ATLAS_URI
+   const uri = process.env.ATLAS_URI_FOR_OWOEYE
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
