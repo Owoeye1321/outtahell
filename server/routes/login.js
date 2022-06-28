ca447e523583c17dd1eda3c581a60d85e03b4874
Owoeye1321 <owoeye1321@gmail.com>
2022-06-28 11:35:46 -0700

    new commit

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,43 +1,3 @@
-<<<<<<< Updated upstream
-if (process.env.NODE_ENV !== "production") require("dotenv").config();
-const uri = process.env.ATLAS_URI;
-
-const router = require("express").Router();
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
-});
-
-router.post("/", (req, res) => {
-  console.log(req.body);
-  const sess = req.session;
-  const username = req.body.username;
-  const password = req.body.password;
-
-  client.connect(async (err) => {
-    console.log("mongodb database connected successfully");
-    const collection = client.db("c_rentals").collection("users");
-    const result = await collection.findOne({
-      username: username,
-      password: password,
-    });
-    if (result) {
-      sess.username = username;
-      res.send("success");
-      console.log("user exist");
-    } else {
-      res.send("invalid");
-      console.log("user does not exist");
-    }
-
-    client.close();
-  });
-});
-
-module.exports = router;
-=======
 const router = require('express').Router()
 const client = require('../controller/client')
 
@@ -64,4 +24,4 @@ router.post('/',async (req, res) =>{
 })
 
 module.exports = router
->>>>>>> Stashed changes
+
