45fee6d81ff1cad77c5d0219d7cc7fac27ee7b31
Owoeye1321 <owoeye1321@gmail.com>
2022-06-28 11:38:36 -0700

    new commit

--- src/components/DashBoard/admin.js
+++ src/components/DashBoard/admin.js
@@ -3,38 +3,39 @@ import { Link } from "react-router-dom";
 import userProfile from "../../assets/images/user.png";
 import logo from "../../assets/images/see.svg";
 import axios from "axios";
-import "./Dashboard.css";
 
 function Admin() {
   const [userDetails, setUserDetails] = useState([]);
 
-  useEffect(() => {
-    const response = async () => {
-      let check = await axios.get("/check");
-      if (check.data === "failed")
-        window.location.assign("http://localhost:3000/login");
-      console.log(check.data);
-    };
-    response();
+  useEffect(()=>{
+    const response = async ()=>{
+        let check = await axios.get('/check');
+        if(check.data ==='failed') window.location.assign('http://localhost:3000/login')
+        console.log(check.data)
+    }
+    response()
 
-    const fetchAll = async () => {
-      const result = await axios.get("/read");
-      if (result.data.length) {
-        setUserDetails(result.data);
-      } else {
-        console.log("Invalid data");
-      }
-    };
-    fetchAll();
-    const interval = setInterval(() => {
-      fetchAll();
-    }, 10000);
+     const fetchAll = async () =>{
+        const result = await axios.get('/read')
+        if(result.data.length){
+            setUserDetails(result.data)
 
-    return () => {
-      clearInterval(interval);
-    };
-  }, []);
-  console.log(userDetails);
+        }else{
+            console.log('Invalid data')
+        }
+
+     }
+     fetchAll()
+        const interval = setInterval (()=>{
+            fetchAll()
+        },10000)
+
+        return()=>{
+                clearInterval(interval)
+        }
+    
+},[])
+console.log(userDetails)
 
   return (
     <>
@@ -44,12 +45,6 @@ function Admin() {
       >
         <div className="row">
           <div className="col-sm-12 col-md-4 col-lg-3 p-4">
-<<<<<<< Updated upstream
-            <div className="pt-5 min-vh-100 test">
-              {userDetails ? (
-                userDetails.map((key) => {
-                  return (
-=======
             <div
               className="pt-5 min-vh-100"
               style={{
@@ -64,68 +59,9 @@ function Admin() {
                  { userDetails.length ? userDetails.map((key)=>{
                    return(
                     
->>>>>>> Stashed changes
                     <div key={key._id}>
-                      <center>
-                        <div
-                          style={{
-                            backgroundColor: "white",
-                            height: "120px",
-                            width: "140px",
-                            borderRadius: "50%",
-                          }}
-                          className="py-3"
-                        >
-                          <img
-                            src={key.imageName}
-                            style={{ height: "90px", width: "90px" }}
-                          />
-                        </div>
-                      </center>
-                      <div className=" mt-5" style={{ color: "white" }}>
-                        <p className="mb-5" style={{ color: "white" }}>
-                          Name: {key.username}
-                        </p>
-                        <p className="mb-5" style={{ color: "white" }}>
-                          Email: {key.email}
-                        </p>
-                        <p className="mb-5" style={{ color: "white" }}>
-                          Phone: {key.phone}
-                        </p>
-                        <p className="mb-5" style={{ color: "white" }}>
-                          Address: {key.address}
-                        </p>
-                        <i>
-                          {" "}
-                          click{" "}
-                          <Link to="/profile" style={{ color: "white" }}>
-                            <input
-                              type="submit"
-                              value="here"
-                              className="form-control bg-primary"
-                              style={{
-                                color: "white",
-                                backgroundColor: "white",
-                              }}
-                            />
-                          </Link>{" "}
-                          to edit profile
-                        </i>
-                        <i className="my-4">
-                          {" "}
-                          Check{" "}
-                          <Link to="/profile" style={{ color: "white" }}>
-                            here
-                          </Link>{" "}
-                          to view gallery
-                        </i>
-                      </div>
-                    </div>
-                  );
-                })
-              ) : (
-                <center>
-                  <div
+                  <center>
+                    <div
                     style={{
                       backgroundColor: "white",
                       height: "120px",
@@ -134,17 +70,10 @@ function Admin() {
                     }}
                     className="py-3"
                   >
+                   
                     <img
-<<<<<<< Updated upstream
-                      src={userProfile}
-                      style={{ height: "90px", width: "90px" }}
-                    />
-                  </div>
-                </center>
-              )}
-=======
                     src={key.imageName}
-                    alt = "Profile Image"
+                    alt = "ProfileImage"
                     style={{ height: "90px", width: "90px" }}
                   />
                   
@@ -203,7 +132,7 @@ function Admin() {
                
                 <img
                 src={userProfile}
-                alt = "Profile Image"
+                alt = "ProfileImage"
                 style={{ height: "90px", width: "90px" }}
               />
               
@@ -252,7 +181,6 @@ function Admin() {
               }
            
             
->>>>>>> Stashed changes
             </div>
           </div>
           <div className="col-sm-12 col-md-8 col-lg-9 p-4">
@@ -261,9 +189,6 @@ function Admin() {
                 className="min-vh-100 d-flex flex-column opacity-mask"
                 style={{ backgroundColor: "rgba(0, 0, 0, 0.05)" }}
               >
-                <div>
-                  <h2>Welcome</h2>
-                </div>
                 <div
                   className="m-5 bg-white px-5 py-3"
                   style={{
@@ -289,7 +214,7 @@ function Admin() {
                       </strong>
                     </figure>
                   </center>
-                  <form>
+                  <form >
                     <div className="form-group">
                       <input
                         className="form-control"
