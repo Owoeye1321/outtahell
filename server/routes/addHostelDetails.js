462f8008118f46339be49b6456f7e11ed014d030
Owoeye1321 <owoeye1321@gmail.com>
2022-07-05 13:24:30 -0700

    new commit

--- server/routes/addHostelDetails.js
+++ server/routes/addHostelDetails.js
@@ -1,7 +1,9 @@
-const router = require('express').json()
+const router = require('express').Router()
 router.post('/',(req, res)=>{
     const sess = req.sesssion
     if(sess.usernaem){
         console.log('Do some stuffs')
+
     }
 })
+module.exports = router
