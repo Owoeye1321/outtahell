3a2faaccccfb25f905ea226bf3209eced5a3fdef
Owoeye1321 <owoeye1321@gmail.com>
2022-08-10 23:32:22 -0700

    new coomit

--- server/routes/signup.js
+++ server/routes/signup.js
@@ -1,6 +1,9 @@
    const validator = require('../controller/validator')
+const profileModel = require('../model/imageModel')
       const router = require('express').Router()
-       const client = require('../controller/client')   
+       const userModel = require('../model/userModel')
+       const fs = require('fs')
+       const path = require('path')
 
 router.post('/',async (req, res) =>{
    console.log(req.body)
@@ -29,44 +32,58 @@ router.post('/',async (req, res) =>{
           data:{err}
      })
      }else{
-      
-      const signUp = async()=>{
-         const collection = client.db("c_rentals").collection("users");
-         const check = await collection.findOne({username:username})
-         if(check){
-            res.send('exist')
-            console.log('user already exist')
-         }else{
- 
-            const result  = await collection.insertOne({username:username, email:email, password:password})
-            if(result){
-               const collection = client.db("c_rentals").collection("admin_profile")
-               const resultProfile = await collection.insertOne({username:username, email:email, password:password})
-               if (resultProfile) {
-                  console.log('added a new profile')
-               } else {
-                  console.log('unable to add a new profile')
-               }
-                   sess.username = username
-                      console.log('user saved')
-                 res.send('success')
-                 
-            }else{
-               res.send('invalid')
-               console.log('unable to save new user')
-            }
-         }
+                  const signUp = new userModel({
+                     username:req.body.username,
+                     email: req.body.email,
+                     password: req.body.password,
+                 })
+                 const profile = new profileModel({
+                  username:req.body.username,
+                  email: req.body.email,
+                  address:"",
+                  phone:"",
+                  image:{
+                        data:fs.readFileSync(path.resolve(__dirname ,'../../src/assets/admin_pictures/user.png')),
+                        contentType:'image/png'
+                  }
+            
+              })
+              
+               //checking if a user exist
+                 userModel.exists({username:username}, (err, result)=>{
+                  if(result){
+                     res.send('exist')
+                     console.log('file exist')
+                  }else{
+                     console.log('file does not exist')
 
-     
-      }
-      signUp()
-  
+                     //saving to sign up page
+                       signUp.save().then((result)=>{
+                           console.log('added a new profile')
+                           console.log('file has been saved to database successfully')
+                           
+                     //saving to profile page
+                     profile.save().then((innerResult)=>{
+                        sess.username = username
+                        console.log('user saved')
+                            res.send('success') 
+                           console.log('user profile has been updated')
+                        }).catch((err)=>{
+                           console.log(err)
+                        })
+
+                    }).catch((err)=>{
+                     console.log(err)
+                     res.send('invalid')
+                    })
+                    
+                  }
+                 })
+                 
+           
+            
      }
   })
-
-
-   
-
 })
 
 module.exports = router
