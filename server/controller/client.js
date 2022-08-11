e96b94b3863ff8cffbd33035f39ebbcdb159543e
Owoeye1321 <owoeye1321@gmail.com>
2022-08-10 23:34:02 -0700

    new commit

--- server/controller/client.js
+++ server/controller/client.js
@@ -1,14 +1,16 @@
 if (process.env.NODE_ENV !== "production") require('dotenv').config();
-   const uri = process.env.ATLAS_URI_FOR_OWOEYE
+   const uri = process.env.ATLAS_URI_FOR_OWOEYE_LOCAL
+   const mongoose = require('mongoose')
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-        client.connect( async err => {
-            client.db("c_rentals")
-            console.log('mongodb database connected successfully')
+    mongoose.connect(uri,  { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })
+    .then(()=>{
+      console.log('connected to database')
+    })
+    .catch(e=>console.log(e));
 
-        });
 
 
 module.exports = client
