a708b46317e856b5e433d76b358eb4b059423791
Owoeye1321 <owoeye1321@gmail.com>
2022-08-10 23:34:31 -0700

    new commit

--- server/routes/contact.js
+++ server/routes/contact.js
@@ -3,18 +3,17 @@
       const nodemailer = require('nodemailer')
 
 router.post('/',(req, res) =>{
-  
+  console.log(req.body)
    
    async function main() {
 
          const fname = req.body.data.fname
                const lname = req.body.data.lname
                   const email = req.body.data.email
-                const phone = req.body.data.phone
+                 const phone = req.body.data.phone
             const subject = req.body.data.subject 
      const message = req.body.data.message
 
-        
                 // create reusable transporter object using the default SMTP transport
                     let transporter = await nodemailer.createTransport({
                       host: "smtp.gmail.com",
@@ -27,23 +26,26 @@ router.post('/',(req, res) =>{
                 },(err, result)=>{
                     if(!err)console.log(result);console.log(err)
                 });
-        
             // send mail with defined transport object
             let info = await transporter.sendMail({
                from:email, // sender address
                     to: "Owoeye1321@gmail.com", // list of receivers
                         subject: subject, // Subject line
                    html:`<h1>Hello C -Rentals</h1> <h5>I'm ${fname + lname} </h5>  <h6>${message} \n  ${phone}</h6>` // plain text body
-              
             },(err, result)=>{
-                if(!err){ console.log(result);res.send('success')}else{console.log(err)}
+                if(!err){ 
+                    console.log('working on sending the emails')
+                       console.log(result);
+                    res.send('success')
+                }else{
+                    console.log('An error has occured and an issue need to be fixed')
+                        console.log(err)
+                    }
             }); 
-        
         // console.log("Message sent: %s", info.messageId);
             // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-        
             // Preview only available when sending through an Ethereal account
-            console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+            // console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
             // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
         }
         
@@ -51,12 +53,3 @@ router.post('/',(req, res) =>{
       });
 
 module.exports = router
-
-
-
-
-
-
-            
-            
-         
