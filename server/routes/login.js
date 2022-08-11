5930b92dd86f099565bd3b54d49f9e595b866dfa
Owoeye1321 <owoeye1321@gmail.com>
2022-08-10 23:32:46 -0700

    new commit

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,25 +1,28 @@
 const router = require('express').Router()
 const client = require('../controller/client')
+const userModel = require('../model/userModel')
 
-router.post('/',async (req, res) =>{
+router.post('/', (req, res) =>{
    console.log(req.body)
    const sess = req.session
          const username = req.body.username
     const password = req.body.password
 
-       const collection = client.db("c_rentals").collection("users");
-             const result = await collection.findOne({ username: username, password: password})
-             console.log('Working on result to find user')
-      if(result){
-         sess.username = username
-             res.send('success')
-         console.log("user exist")
-      } else {
-         res.send('invalid')
-         console.log('user does not exist')
-      }
-     
-   
+    const login = new userModel({
+      username:username,
+      password:password,
+  })
+  //checking if a user exist
+      userModel.exists({username:username}, (err, result)=>{
+         if(result){
+            sess.username = username
+            res.send('success')
+            console.log('The user exist')
+         }else{
+            res.send('invalid')
+            console.log('The user does not exist')
+         }
+      })
 
 })
 
