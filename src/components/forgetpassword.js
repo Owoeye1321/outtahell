a5875840255d0caa72323c5685241163676f1f69
Owoeye1321 <owoeye1321@gmail.com>
2022-08-10 23:15:27 -0700

    new commit

--- src/components/forgetpassword.js
+++ src/components/forgetpassword.js
@@ -1,7 +1,29 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { motion } from 'framer-motion/dist/framer-motion'
+import {useState} from 'react'
+import axios from 'axios'
 
 function Login() {
+    const [email, setEmail] = useState({
+        email:''
+    })
+    const handle = (e)=>{
+        const data = { ...email }
+        data[e.target.id] = e.target.value
+        setEmail(data)
+        console.log(email)
+    }
+
+    const submitThisShit = async (e)=>{
+        e.preventDefault()
+        const result = await axios.post('/forgetpassword',{
+            email:email.email
+        })
+        if(result){
+            console.log('working on the result')
+            console.log(result)
+        }
+    }
     return (
         <motion.div
         initial = {{opacity: 0 }}
@@ -12,14 +34,19 @@ function Login() {
                 <div className='col-sm-12 col-md-4 col-lg-4'>
 
                 </div>
-                <div className='col-sm-12 col-md-4 col-lg-4' style = {{padding:'150px 50px 50px 50px'}}>
-	                        <form className="input_style_1" method="post">
-	                            <div id="forgot_pw">
-	                                <h4 className="mb-3">Forgot Password</h4>
-	                                <div className="form-group">
-	                                    <label for="email_forgot">Login email</label>
-	                                    <input type="email" className="form-control" name="email_forgot" id="email_forgot"/>
-	                                </div>
+                <div className='col-sm-12 col-md-4 col-lg-4 my-5'>
+	                        <form className="input_style_1" method="post" onSubmit={(e)=>{submitThisShit(e)}}>
+	                            <div id="forgot_pw my-5">
+	                                <h4 className="my-5">Forgot Password</h4>
+                                    <div className="form-group mb-4">
+              <input
+                onChange={(e) => handle(e)}
+                type="email"
+                placeholder="Email address"
+                id="email"
+                className="form-control"
+              />
+            </div>
 	                                <p>You will receive an email containing a link allowing you to reset your password to a new preferred one.</p>
 	                                <div className="text-center"><input type="submit" value="Reset Password" className="form-control "/></div>
 	                            </div>
