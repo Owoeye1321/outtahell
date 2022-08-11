47f4cf47a0912566db302b936930dbd7a2fa3668
Owoeye1321 <owoeye1321@gmail.com>
2022-08-10 23:04:21 -0700

    new commit

--- server/app.js
+++ server/app.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const session = require("express-session");
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(express.json())
+const path = require('path')
 //const fileUpload = require("express-fileupload");
 //const path = require('path')
 const PORT = process.env.PORT || 9000;
-const bodyParser = require("body-parser");
+
 // combines the 2 above, then you can parse incoming Request Object if object, with nested objects, or generally any type.
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static( path.resolve(__dirname, '../../src/assets/admin_pictures')));
+app.use(express.static( path.resolve(__dirname,  '../../src/assets/hostel_pages')));
 // calling body-parser to handle the Request Object from POST requests
 const oneDay = 1000 * 60 * 60 * 24;
 app.use(
@@ -22,6 +27,7 @@ app.use(
     resave: false,
   })
 );
+//app.use('/testingImage', require('./routes/testingImage'))
 app.use("/forgetpassword", require("./routes/forgetpassword"));
 app.use("/login", require("./routes/login"));
 app.use("/signup", require("./routes/signup"));
@@ -31,6 +37,7 @@ app.use("/check", require("./routes/check"));
 app.use("/read", require("./routes/readProfile"));
 app.use('/logOut', require('./routes/logOut'))
 app.use('/addHostelDetails', require('./routes/addHostelDetails'))
+app.use('/getAllHostelInfo', require('./routes/fetchAllHostelDetails'))
 
 app.all("*", (req, res) => {
   res.send("Hello there, you seem to be lost on this server");
